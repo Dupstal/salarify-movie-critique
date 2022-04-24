a889cd2b4d09476544c64bba5ee302bb6a15a402
Nandor Gyorgy Feher <dupstal@gmail.com>
2022-04-24 17:22:39 +0200

    Expand backend movie controller

--- application/backend/controllers/movie.js
+++ application/backend/controllers/movie.js
@@ -5,7 +5,24 @@ let movies = JSON.parse(tempData);
 
 exports.getMovies = async (req, res, next) => {
   try {
-    return res.status(200).json(movies);
+    let page = +req.query.page;
+    if (!page) {
+      page = 0;
+    }
+
+    const moviesToReturn = movies.slice(page * 6, (page + 1) * 6);
+
+    return res.status(200).json(moviesToReturn);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send();
+  }
+}
+
+exports.getNumberOfPages = async (req, res, next) => {
+  try {
+    const numberOfPages = Math.ceil(movies.length / 6);
+    return res.status(200).json(numberOfPages);
   } catch (error) {
     console.log(error);
     return res.status(500).send();
--- application/backend/routes/movie.js
+++ application/backend/routes/movie.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", MovieController.getMovies);
+router.get("/pages", MovieController.getNumberOfPages);
 
 module.exports = router;
